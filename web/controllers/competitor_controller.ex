e600a0d51df1849c4744768771a8a6b9e2a827a4
Sean Chen <skypemaster007@gmail.com>
2016-01-14 20:38:35 -0800

    Add authentication as a plug in competitor_controller

--- web/controllers/competitor_controller.ex
+++ web/controllers/competitor_controller.ex
@@ -1,9 +1,10 @@
 defmodule GenComp.CompetitorController do
     use GenComp.Web, :controller
+    plug :authenticate when action in [:index, :show]
     alias GenComp.Competitor
 
     def index(conn, _params) do
-        competitors = Repo.all(GenComp.Competitor)
+        competitors = Repo.all(Competitor)
         render conn, "index.html", competitors: competitors
     end
 
@@ -26,6 +27,17 @@ defmodule GenComp.CompetitorController do
                 |> redirect(to: competitor_path(conn, :index))
             {:error, changeset} ->
                 render(conn, "new.html", changeset: changeset)
+        end
+    end
+
+    defp authenticate(conn, _opts) do
+        if conn.assigns.current_competitor do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You must be logged in to access that page.")
+            |> redirect(to: page_path(conn, :index))
+            |> halt()
         end
     end
 end
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule GenComp.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug GenComp.Auth, repo: GenComp.Repo
   end
 
   pipeline :api do
